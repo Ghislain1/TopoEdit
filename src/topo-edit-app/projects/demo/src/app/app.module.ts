8e2a9c4c3a66a70424d86c2f0f975567339039dd
Ghis <Gzeleu@protonmail.com>
2019-04-28 14:25:57 +0200

    Init projects

--- src/topo-edit-app/projects/demo/src/app/app.module.ts
+++ src/topo-edit-app/projects/demo/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AngularTopoEditorModule } from 'projects/angular-topo-editor/src/lib/angular-topo-editor.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularTopoEditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
