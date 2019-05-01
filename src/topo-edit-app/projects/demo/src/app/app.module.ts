2e2148a6cb44476ae5c669c06eedaeb712c7064e
Ghis <Gzeleu@protonmail.com>
2019-05-01 17:10:53 +0200

    Implemented angular bootstrap nav bar

--- src/topo-edit-app/projects/demo/src/app/app.module.ts
+++ src/topo-edit-app/projects/demo/src/app/app.module.ts
@@ -1,9 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { LoginComponent } from './shared/components/login/login.component';
+import { HomeComponent } from './shared/components/home/home.component';
 // import { AngularTopoEditorModule } from 'projects/angular-topo-editor/src/lib/angular-topo-editor.module';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'home/:id', component: LoginComponent },
+  {
+    path: 'heroes',
+    component: LoginComponent,
+    data: { title: 'Heroes List' }
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: '**', component: LoginComponent }
+];
 
 @NgModule({
   declarations: [
@@ -11,6 +30,10 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     SharedModule
     // AngularTopoEditorModule
   ],
--- src/topo-edit-app/projects/demo/src/app/shared/shared.module.ts
+++ src/topo-edit-app/projects/demo/src/app/shared/shared.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [NavbarComponent, HomeComponent, LoginComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
   ],
   exports: [NavbarComponent]
 })
